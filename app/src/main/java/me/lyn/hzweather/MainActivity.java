d855272e686979a0f0f511b9310a949a4911789e
Xiangyang <linxiangyang@lianfan.com>
2017-04-09 11:48:47 +0800

    first commit

---
+++ app/src/main/java/me/lyn/hzweather/MainActivity.java
@@ -0,0 +1,31 @@
+package me.lyn.hzweather;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWebView = (WebView) findViewById(R.id.webview);
+        mWebView.setWebViewClient(new WebViewClient());
+
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.loadUrl("https://www.baidu.com/s?word=杭州天气");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
